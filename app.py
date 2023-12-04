d9c51fe593f691701e519344da0bb76ec3cc565f
20006343 <user@user-HP-EliteBook-8460p.localhost>
2023-12-04 12:30:29 +0300

    Updated Project Structure

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
-    return "Hello World!"
+    return "Api sample"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
